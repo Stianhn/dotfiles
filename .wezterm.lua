c6d9c487bc9b0b6d3753700418bf8e1aba08acbf
Stian H. Nesbø <stian.n@gmail.com>
2024-04-04 15:41:47 -0500

    wez - hide tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,5 +8,6 @@ local config = wezterm.config_builder()
 config.font_size = 16
 config.color_scheme = "Catppuccin Mocha"
 
+config.hide_tab_bar_if_only_one_tab = true
 -- and finally, return the configuration to wezterm
 return config
