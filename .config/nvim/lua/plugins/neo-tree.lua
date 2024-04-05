e811f3fa6c1cd1a304582c5ef781ab9f00d7fecf
Stian H. Nesbø <stian.n@gmail.com>
2024-04-05 11:01:52 -0500

    vim - neotree update config -refactor -follow current buffer on toggle -close when last buffer

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,24 +7,36 @@ return {
     'MunifTanjim/nui.nvim',
     '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  config = function()
-    vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'File Explorer' })
-    require('neo-tree').setup {
-      window = {
-        mappings = {
-          ['o'] = 'open',
-          -- Unbinding these is necesarry to prevent a delay when using o
-          -- TODO: reseach what the keybindings bellow does.
-          --
-          ['oc'] = 'noop',
-          ['od'] = 'noop',
-          ['og'] = 'noop',
-          ['om'] = 'noop',
-          ['on'] = 'noop',
-          ['os'] = 'noop',
-          ['ot'] = 'noop',
-        },
+  keys = {
+    {
+      '<leader>e',
+      '<Cmd>Neotree toggle<CR>',
+      { desc = 'File Explorer' },
+    },
+  },
+  opts = {
+    close_if_last_window = true,
+    window = {
+      mappings = {
+        ['o'] = 'open',
+        -- Unbinding these is necesarry to prevent a delay when using o
+        -- TODO: reseach what the keybindings bellow does.
+        --
+        ['oc'] = 'noop',
+        ['od'] = 'noop',
+        ['og'] = 'noop',
+        ['om'] = 'noop',
+        ['on'] = 'noop',
+        ['os'] = 'noop',
+        ['ot'] = 'noop',
       },
-    }
+    },
+    filesystem = {
+      bind_to_cwd = true,
+      follow_current_file = { enabled = true },
+    },
+  },
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
   end,
 }
