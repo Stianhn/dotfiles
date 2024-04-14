01bf7c6bdd7630893865cfba1f78889e2f67f9f8
Stian H. Nesbø <stian.n@gmail.com>
2024-04-14 13:00:55 -0500

    vim - refactor

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
+  end,
+}
