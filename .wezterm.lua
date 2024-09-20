7029ab84c7dcdd2555dfe3b8d9c46c23f75dc88a
stianhn <stian.n@gmail.com>
2024-09-20 14:28:48 +0200

    wezterm - disable blinking cursor

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.cursor_blink_rate = 0
+
 -- This is where you actually apply your config choices
 config.font_size = 16
 config.color_scheme = "Catppuccin Mocha"
@@ -12,3 +14,4 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 -- and finally, return the configuration to wezterm
 return config
+
