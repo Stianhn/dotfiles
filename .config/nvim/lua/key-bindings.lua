471bb941a57d607177f12c2a94eb2a2bb4db47ec
Stian <stian.n@gmail.com>
2026-01-30 15:01:56 -0500

    nvim - keep registry while pasting in visual mode

--- .config/nvim/lua/key-bindings.lua
+++ .config/nvim/lua/key-bindings.lua
@@ -29,3 +29,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Paste over without losing the register
+vim.keymap.set('v', 'p', '"_dP')
